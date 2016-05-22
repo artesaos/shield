5b92a7b418f6dda9ea4e6c7df3c696ac3c17bab4
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-05-22 01:44:03 -0300

    add facade

---
+++ src/Facades/Shield.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Artesaos\Shield\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Shield extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'opengraph';
+    }
+}
